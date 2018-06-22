a1458319f41e58080cbe6cf84940c87cfb668b65
Victor Chen <yvictorck@gmail.com>
2018-06-22 04:15:56 -0700

    a

--- Src/dispatch.cpp
+++ Src/dispatch.cpp
@@ -5032,7 +5032,7 @@ CL_API_ENTRY cl_int CL_API_CALL CLIRN(clEnqueueNDRangeKernel)(
                 }
             }
 
-
+// //meow
 // int vic;
 // puts ("Enter text. Include a dot ('.') in a sentence to exit:");
 // do {
@@ -5043,77 +5043,77 @@ CL_API_ENTRY cl_int CL_API_CALL CLIRN(clEnqueueNDRangeKernel)(
 //   cout << "value of a: " << a << endl;
 // }
 
-    int client_sock, rc, len;
-    struct sockaddr_un server_sockaddr; 
-    struct sockaddr_un client_sockaddr; 
-    char buf[256];
-    memset(&server_sockaddr, 0, sizeof(struct sockaddr_un));
-    memset(&client_sockaddr, 0, sizeof(struct sockaddr_un));
-    client_sock = socket(AF_UNIX, SOCK_STREAM, 0);
-    // if (client_sock == -1) {
-    //     printf("SOCKET ERROR = %d\n", 88);
-    //     exit(1);
-    // }
-        client_sockaddr.sun_family = AF_UNIX;   
-    strcpy(client_sockaddr.sun_path, CLIENT_PATH); 
-    len = sizeof(client_sockaddr);
+    // int client_sock, rc, len;
+    // struct sockaddr_un server_sockaddr; 
+    // struct sockaddr_un client_sockaddr; 
+    // char buf[256];
+    // memset(&server_sockaddr, 0, sizeof(struct sockaddr_un));
+    // memset(&client_sockaddr, 0, sizeof(struct sockaddr_un));
+    // client_sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    // // if (client_sock == -1) {
+    // //     printf("SOCKET ERROR = %d\n", 88);
+    // //     exit(1);
+    // // }
+    //     client_sockaddr.sun_family = AF_UNIX;   
+    // strcpy(client_sockaddr.sun_path, CLIENT_PATH); 
+    // len = sizeof(client_sockaddr);
     
-    unlink(CLIENT_PATH);
-    rc = bind(client_sock, (struct sockaddr *) &client_sockaddr, len);
-    // if (rc == -1){
-    //     printf("BIND ERROR: %d\n", 88);
-    //     close(client_sock);
-    //     exit(1);
-    // }
-    /* Set up the UNIX sockaddr structure  */
-    /* for the server socket and connect   */
-    /* to it.                              */
-    /***************************************/
-    server_sockaddr.sun_family = AF_UNIX;
-    strcpy(server_sockaddr.sun_path, SERVER_PATH);
-    rc = connect(client_sock, (struct sockaddr *) &server_sockaddr, len);
-    // if(rc == -1){
-    //     printf("CONNECT ERROR = %d\n", 88);
-    //     close(client_sock);
-    //     exit(1);
-    // }
+    // unlink(CLIENT_PATH);
+    // rc = bind(client_sock, (struct sockaddr *) &client_sockaddr, len);
+    // // if (rc == -1){
+    // //     printf("BIND ERROR: %d\n", 88);
+    // //     close(client_sock);
+    // //     exit(1);
+    // // }
+    // /* Set up the UNIX sockaddr structure  */
+    // /* for the server socket and connect   */
+    // /* to it.                              */
+    // /***************************************/
+    // server_sockaddr.sun_family = AF_UNIX;
+    // strcpy(server_sockaddr.sun_path, SERVER_PATH);
+    // rc = connect(client_sock, (struct sockaddr *) &server_sockaddr, len);
+    // // if(rc == -1){
+    // //     printf("CONNECT ERROR = %d\n", 88);
+    // //     close(client_sock);
+    // //     exit(1);
+    // // }
     
-    /************************************/
-    /* Copy the data to the buffer and  */
-    /* send it to the server socket.    */
-    /************************************/
-    strcpy(buf, DATA);                 
-    // printf("Sending data...\n");
-    rc = send(client_sock, buf, strlen(buf), 0);
-    // if (rc == -1) {
-    //     printf("SEND ERROR = %d\n", 88);
-    //     close(client_sock);
-    //     exit(1);
-    // }   
-    // else {
-    //     printf("Data sent!\n");
-    // }
+    // /************************************/
+    // /* Copy the data to the buffer and  */
+    // /* send it to the server socket.    */
+    // /************************************/
+    // strcpy(buf, DATA);                 
+    // // printf("Sending data...\n");
+    // rc = send(client_sock, buf, strlen(buf), 0);
+    // // if (rc == -1) {
+    // //     printf("SEND ERROR = %d\n", 88);
+    // //     close(client_sock);
+    // //     exit(1);
+    // // }   
+    // // else {
+    // //     printf("Data sent!\n");
+    // // }
 
-    /**************************************/
-    /* Read the data sent from the server */
-    /* and print it.                      */
-    /**************************************/
-    // printf("Waiting to recieve data...\n");
-    memset(buf, 0, sizeof(buf));
-    rc = recv(client_sock, buf, sizeof(buf),MSG_PEEK);
-    // if (rc == -1) {
-    //     printf("RECV ERROR = %d\n", 88);
-    //     close(client_sock);
-    //     exit(1);
-    // }   
-    // else {
-    //     printf("DATA RECEIVED = %s\n", buf);
-    // }
+    // /**************************************/
+    // /* Read the data sent from the server */
+    // /* and print it.                      */
+    // /**************************************/
+    // // printf("Waiting to recieve data...\n");
+    // memset(buf, 0, sizeof(buf));
+    // rc = recv(client_sock, buf, sizeof(buf),MSG_PEEK);
+    // // if (rc == -1) {
+    // //     printf("RECV ERROR = %d\n", 88);
+    // //     close(client_sock);
+    // //     exit(1);
+    // // }   
+    // // else {
+    // //     printf("DATA RECEIVED = %s\n", buf);
+    // // }
     
-    /******************************/
-    /* Close the socket and exit. */
-    /******************************/
-    close(client_sock);
+    // /******************************/
+    // /* Close the socket and exit. */
+    // /******************************/
+    // close(client_sock);
 
             CALL_LOGGING_ENTER_KERNEL(
                 kernel,
