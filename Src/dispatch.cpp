d10efa555d373782c7ca5d52799b60b26d50a550
Victor Chen <yvictorck@gmail.com>
2018-06-27 07:57:00 -0700

    a

--- Src/dispatch.cpp
+++ Src/dispatch.cpp
@@ -73,6 +73,7 @@ using namespace std;
 #include <mqueue.h>
 
 #define SERVER_QUEUE_NAME   "/sp-example-server"
+#define CLI_QUEUE_NAME   "/sp-example-cli"
 #define QUEUE_PERMISSIONS 0660
 #define MAX_MESSAGES 10
 #define MAX_MSG_SIZE 256
@@ -5084,30 +5085,39 @@ CL_API_ENTRY cl_int CL_API_CALL CLIRN(clEnqueueNDRangeKernel)(
 char client_queue_name [64];
     mqd_t qd_server, qd_client;   // queue descriptors
     // create the client queue for receiving messages from server
-    sprintf (client_queue_name, "/sp-example-client-%d", getpid ());
+    // sprintf (client_queue_name, "/sp-example-client-%d", getpid ());
     struct mq_attr attr;
     attr.mq_flags = 0;
     attr.mq_maxmsg = MAX_MESSAGES;
     attr.mq_msgsize = MAX_MSG_SIZE;
     attr.mq_curmsgs = 0;
-    if ((qd_client = mq_open (client_queue_name, O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &attr)) == -1) {
-        perror ("Client: mq_open (client)");
-        exit (1);
-    }
+    // if ((qd_client = mq_open (client_queue_name, O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &attr)) == -1) {
+    //     perror ("Client: mq_open (client)");
+    //     exit (1);
+    // }
+    // if ((qd_client = mq_open (CLI_QUEUE_NAME, O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &attr)) == -1) {
+    //     perror ("Client: mq_open (client)");
+    //     exit (1);
+    // }
     if ((qd_server = mq_open (SERVER_QUEUE_NAME, O_WRONLY)) == -1) {
         perror ("Client: mq_open (server)");
         exit (1);
     }
     char in_buffer [MSG_BUFFER_SIZE];
     char temp_buf [10];
-    mq_send (qd_server, client_queue_name, strlen (client_queue_name) + 1, 0);
+    // mq_send (qd_server, client_queue_name, strlen (client_queue_name) + 1, 0);
+    mq_send (qd_server, CLI_QUEUE_NAME, strlen (CLI_QUEUE_NAME) + 1, 0);
     mq_receive (qd_client, in_buffer, MSG_BUFFER_SIZE, NULL);
     if (mq_close (qd_client) == -1) {
         perror ("Client: mq_close");
         exit (1);
     }
 
-    if (mq_unlink (client_queue_name) == -1) {
+    // if (mq_unlink (client_queue_name) == -1) {
+    //     perror ("Client: mq_unlink");
+    //     exit (1);
+    // }
+    if (mq_unlink (CLI_QUEUE_NAME) == -1) {
         perror ("Client: mq_unlink");
         exit (1);
     }
