125127de7e31284b7057c179b5d5dd72f95634fc
Victor Chen <yvictorck@gmail.com>
2018-06-27 01:31:12 -0700

    a

--- Src/dispatch.cpp
+++ Src/dispatch.cpp
@@ -5059,16 +5059,16 @@ CL_API_ENTRY cl_int CL_API_CALL CLIRN(clEnqueueNDRangeKernel)(
     client_sockaddr.sun_family = AF_UNIX; 
 
 
-    char hostname[10];
-    char hostname2[20];
+    char hostname[18];
+
     char const * pre = "/foo/";
     printf("        %s\n",pre);
-    gethostname(hostname, 10);
-    gethostname(hostname2, 20);
+    gethostname(hostname, 18);
+
     printf("        %s\n",hostname);
-    printf("        %s\n",hostname2);
-    // memmove(hostname + 4, hostname, strlen(hostname) + 1);
-    // printf("        meow%s",hostname);
+
+    memmove(hostname + 4, hostname, 12);
+    printf("        meow%s",hostname);
 
     // memcpy(hostname,pre,4);
     // printf("        meow meow%s",hostname);
