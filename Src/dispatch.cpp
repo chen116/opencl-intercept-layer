4c3b92d4641b0ae2f62d7f62d1de1bff070473a0
Victor Chen <yvictorck@gmail.com>
2018-06-29 00:58:41 -0700

    a

--- Src/dispatch.cpp
+++ Src/dispatch.cpp
@@ -5070,62 +5070,95 @@ CL_API_ENTRY cl_int CL_API_CALL CLIRN(clEnqueueNDRangeKernel)(
 
 // boost sema
 
+
     struct shared_memory_buffer
-        {
-           enum { NumItems = 1 };
-           shared_memory_buffer()
-              : mutex(1), nempty(NumItems), nstored(0)
-           {}
-           //Semaphores to protect and synchronize access
-           boost::interprocess::interprocess_semaphore
-              mutex, nempty, nstored;
-           //Items to fill
-           int items[NumItems];
-        };
+    {
+       enum { NumItems = 5 };
 
-    shared_memory_object docker_shm
-          (create_only//only create
-          ,"docker_MySharedMemory"              //name
-          ,read_write  //read-write mode
-          );
-   docker_shm.truncate(sizeof(shared_memory_buffer));
+       shared_memory_buffer()
+          : mutex(1), nempty(NumItems), nstored(0)
+       {}
 
-       mapped_region docker_region
-          (docker_shm                       //What to map
-          ,read_write //Map it as read-write
-          );
-       //Get the address of the mapped region
-       void * docker_addr       = docker_region.get_address();
-       //Construct the shared structure in memory
-       shared_memory_buffer * docker_data = new (docker_addr) shared_memory_buffer;
+       //Semaphores to protect and synchronize access
+       boost::interprocess::interprocess_semaphore
+          mutex, nempty, nstored;
+
+       //Items to fill
+       int items[NumItems];
+    };
+   //Remove shared memory on destruction
+
+   struct shm_remove
+   {
+      ~shm_remove(){ shared_memory_object::remove("MySharedMemory");shared_memory_object::remove("vic_MySharedMemory"); }
+   } remover;
+
+   //Create a shared memory object.
+   shared_memory_object vic_shm
+      (open_only                  //only create
+      ,"vic_MySharedMemory"              //name
+      ,read_write  //read-write mode
+      );
+   //Map the whole shared memory in this process
+   mapped_region vic_region
+      (vic_shm                       //What to map
+      ,read_write //Map it as read-write
+      );
+
+   //Get the address of the mapped vic_region
+   void * vic_addr       = vic_region.get_address();
+
+   //Construct the shared structure in memory
+   shared_memory_buffer * vic_data = new (vic_addr) shared_memory_buffer;
 
 
-    shared_memory_object shm
-          (open_only//only create
-          ,"MySharedMemory"              //name
-          ,read_write  //read-write mode
-          );
-       mapped_region region
-          (shm                       //What to map
-          ,read_write //Map it as read-write
-          );
-       //Get the address of the mapped region
-       void * addr       = region.get_address();
-       //Construct the shared structure in memory
-       shared_memory_buffer * data = new (addr) shared_memory_buffer;
-          data->nempty.wait();
-          data->mutex.wait();
-          data->items[0] = 66;
-          printf("docker wrote: %d\n",66 );
-          data->mutex.post();
-          data->nstored.post();
-          printf("cryyy\n");
-       //Extract the data
-       docker_data->nstored.wait();
-       docker_data->mutex.wait();
-       printf("docker got: %d\n",docker_data->items[0]);
-       docker_data->mutex.post();
-       docker_data->nempty.post();
+
+
+
+   //Create a shared memory object.
+   shared_memory_object shm
+      (open_only                    //only create
+      ,"MySharedMemory"              //name
+      ,read_write  //read-write mode
+      );
+
+   //Map the whole shared memory in this process
+   mapped_region region
+      (shm                       //What to map
+      ,read_write //Map it as read-write
+      );
+
+   //Get the address of the mapped region
+   void * addr       = region.get_address();
+
+   //Obtain the shared structure
+   shared_memory_buffer * data = static_cast<shared_memory_buffer*>(addr);
+
+   const int NumMsg = 10;
+
+   int extracted_vic_data [NumMsg];
+   //Insert data in the array
+   for(int i = 0; i < NumMsg; ++i){
+      data->nempty.wait();
+      data->mutex.wait();
+      data->items[i % shared_memory_buffer::NumItems] = i+10;
+      printf("wrote: %d\n",i );
+      data->mutex.post();
+      data->nstored.post();
+   }
+
+   //Extract the data
+   for(int i = 0; i < NumMsg; ++i){
+      vic_data->nstored.wait();
+      vic_data->mutex.wait();
+      extracted_vic_data[i] = vic_data->items[i % shared_memory_buffer::NumItems];
+      printf("got: %d\n",extracted_vic_data[i]);
+      vic_data->mutex.post();
+      vic_data->nempty.post();
+   }
+
+
+
 
 //mq
     // mqd_t qd_server, qd_client;   // queue descriptors
